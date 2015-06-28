494f2fc113e52933fa37b33c0da19c46a309fd2e
Tailszefox <tails@tailszefox.no-ip.com>
2015-06-28 13:19:49 +0200

    Y'a plusieurs résumés

--- php/log.php
+++ php/log.php
@@ -23,7 +23,7 @@ krsort($logs);
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
 	<head>
-		<title>Loups-Garous - Résumé des parties</title>
+		<title>Loups-Garous - Résumés des parties</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<link rel="stylesheet" media="screen" type="text/css" title="Style" href="style.css" />
 	</head>
