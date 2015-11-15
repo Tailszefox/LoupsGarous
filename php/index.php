de3efd9c9506e02d94f1fa9206815f2067e38eda
Tailszefox <tails@tailszefox.net>
2015-11-15 12:36:31 +0100

    index.php: ajout titre

--- php/index.php
+++ php/index.php
@@ -4,7 +4,7 @@ include_once('../is_connected.php');
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
 	<head>
-		<title>Loups-Garous</title>
+		<title>Loups-Garous - Personnalités</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<link rel="stylesheet" media="screen" type="text/css" title="Style" href="style.css" />
 		<script type="text/javascript" src="jquery.js"></script>
