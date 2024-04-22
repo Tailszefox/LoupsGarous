3549bc9014c09d80b6a2fd4e06766be825bdb439
Tailszefox <tails@tailszefox.net>
2024-04-22 22:06:56 +0200

    Import du bon module lors du setup

--- setup/check_irclib.py
+++ setup/check_irclib.py
@@ -4,10 +4,10 @@
 import sys
 
 try:
-    import irclib
-    print("Le module irclib est installé.")
+    import irc.client as irclib
+    print("Le module irc est installé.")
 except ImportError:
-    print("Le module irclib n'est pas installé. Veuillez installer le module pour la version actuelle de Python 2.")
+    print("Le module irc n'est pas installé. Veuillez installer le module pour la version actuelle de Python 2.")
     print("Si pip est installé, vous pouvez exécuter la commande suivante :")
     print("pip2 install git+https://github.com/jaraco/irc.git@c4097183e57ffef083e0fc185a377eeca7454725")
     sys.exit(1)
